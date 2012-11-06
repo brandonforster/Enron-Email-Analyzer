3292f9879e8c36ab9340efc0eeef020faa862030
Brandon Forster <brandonforster@gmail.com>
2012-11-05 22:24:50 -0500

    added header and fixed code

--- src/graphjam/Graph.java
+++ src/graphjam/Graph.java
@@ -1,5 +1,16 @@
+//Brandon Forster, Robert Millward,
+//William Corlett and Phillip Rajala
+//COP 3503 Assignment 3
+//Enron E-mail Graphing
+//11 November 2012
+//Graph Object
+
+package graphjam;
+
+import java.util.*;
+
 public class Graph {
-	private ArrayList<Node> nodes = new ArrayList<Node>;
+	private ArrayList<Node> nodes = new ArrayList<Node>();
 	
 	public void addNode(Node node){
 		nodes.add(node);
--- src/graphjam/Node.java
+++ src/graphjam/Node.java
@@ -1,8 +1,17 @@
+//Brandon Forster, Robert Millward,
+//William Corlett and Phillip Rajala
+//COP 3503 Assignment 3
+//Enron E-mail Graphing
+//11 November 2012
+//Node Object
+
 package graphjam;
+
+import java.util.*;
 
 public class Node {
 	private int id;
-	private ArrayList<Integer> edges = new ArrayList<Integer>;
+	private ArrayList<Integer> edges = new ArrayList<Integer>();
 
 	public Node(int id){
 		this.id  = id;
