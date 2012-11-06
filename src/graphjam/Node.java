b5438c5c607bef2c4dda54636efcc3a16daa9a58
Brandon Forster <brandonforster@gmail.com>
2012-11-05 23:12:40 -0500

    Revert "ArrayLists what what"
    This reverts commit 8b975ae4b4624a6d37f6ba8f5823815e9067940f.

--- src/graphjam/Node.java
+++ src/graphjam/Node.java
@@ -1,24 +1,5 @@
 package graphjam;
 
 public class Node {
-	private int id;
-	private ArrayList<Integer> edges = new ArrayList<Integer>;
 
-	public Node(int id){
-		this.id  = id;
-	}
-
-	public int getId(){
-		return id;
-	}
-	
-	public void addEdge(int edge)
-	{
-		edges.add(edge);
-	}
-	
-	public ArrayList<Integer> getEdges()
-	{
-		return edges;
-	}
 }
