8b975ae4b4624a6d37f6ba8f5823815e9067940f
Robert Millward <robert.millward@knights.ucf.edu>
2012-11-05 21:08:20 -0500

    ArrayLists what what

---
+++ src/graphjam/Graph.java
@@ -0,0 +1,13 @@
+public class Graph {
+	private ArrayList<Node> nodes = new ArrayList<Node>;
+	
+	public void addNode(Node node){
+		nodes.add(node);
+	}
+	
+	public int size() {
+		return nodes.size();
+	}
+	
+	
+}
--- src/graphjam/Node.java
+++ src/graphjam/Node.java
@@ -1,5 +1,24 @@
 package graphjam;
 
 public class Node {
+	private int id;
+	private ArrayList<Integer> edges = new ArrayList<Integer>;
 
+	public Node(int id){
+		this.id  = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+	
+	public void addEdge(int edge)
+	{
+		edges.add(edge);
+	}
+	
+	public ArrayList<Integer> getEdges()
+	{
+		return edges;
+	}
 }
